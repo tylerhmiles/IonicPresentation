e916ad0b4296514b863404a9ce1561be3d298b5e
Phi Khanh Hoang <94206447+PhiKhanhHoang@users.noreply.github.com>
2023-04-12 21:59:15 -0400

    added features on tab1

--- ionic-presentation-app/src/pages/Tab1.tsx
+++ ionic-presentation-app/src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonButton, IonAlert,IonInput, IonItem, IonList,IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -10,18 +10,54 @@ const Tab1: React.FC = () => {
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
+   
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <IonContent class="ion-padding">
-          <h1>Example App</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum repellat officiis ex natus nulla deserunt cum repudiandae nostrum. Odit harum eos perferendis sit vero minus molestiae minima non perspiciatis officiis.</p>
+        </IonHeader>   
+     
+        <IonContent class="content">
+          <h1>Demo Application</h1>
         </IonContent>
+      
+       
+   <IonList>
+   <h1>Ion components implemented <br/></h1>
+      <IonItem>
+        <IonLabel>Ion cards</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Ion input</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Ion Buttons</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Ion Button</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Ion Content</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Ion Alert </IonLabel>
+      </IonItem>
+    </IonList>
+    <IonButton  id="present-alert">Alert Demo</IonButton>
+    <IonButton onclick="window.open('https://ionicframework.com/docs', '_system', 'location=yes'); return false;">Official documentation</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="Demo"
+        subHeader="Test message"
+        message="This is a demo of the alert function "
+        buttons={['OK']}
+      ></IonAlert>
+     
       </IonContent>
-    </IonPage>
+      
+  
+    </IonPage>  
   );
 };
 
