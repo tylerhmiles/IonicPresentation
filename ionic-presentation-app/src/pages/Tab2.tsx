b697973937ab154bb9b989629d96673199956fd5
Phi Khanh Hoang <94206447+PhiKhanhHoang@users.noreply.github.com>
2023-04-12 21:56:46 -0400

    added features to tab1

--- ionic-presentation-app/src/pages/Tab2.tsx
+++ ionic-presentation-app/src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -7,12 +7,14 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          
           <IonTitle>Tab 2</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+
         <IonHeader collapse="condense">
-          <IonToolbar>
+          <IonToolbar> 
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
